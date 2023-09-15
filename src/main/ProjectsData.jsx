d00ed2e779963b53e4dc2734ce7e61cefdeb43e9
houssame errjem <errjem2@gmail.com>
2023-09-15 17:37:01 +0100

    commit

--- src/main/ProjectsData.jsx
+++ src/main/ProjectsData.jsx
@@ -1,15 +1,15 @@
-import story_corner_img from "./images/story-corner.png";
+// import story_corner_img from "./images/story-corner.png";
 import solar_system_img from "./images/solar-system.png";
 import liverpool_fc_img from "./images/liverpool.png";
 
 const ProjectsData = [
-  {
-    title: "Story Corner",
-    description: "Modern blogging, share, like, manage stories.",
-    image: story_corner_img,
-    url: "https://story-corner.netlify.app/",
-    code: "https://github.com/Errjem/story-corner",
-  },
+  // {
+  //   title: "Story Corner",
+  //   description: "Modern blogging, share, like, manage stories.",
+  //   image: story_corner_img,
+  //   url: "https://story-corner.netlify.app/",
+  //   code: "https://github.com/Errjem/story-corner",
+  // },
   {
     title: "Solar System",
     description: "Explore interactive Solar System planets knowledge.",
