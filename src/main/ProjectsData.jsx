c6c95ad983b1cfedebfc938b7b750bf859824d30
houssame errjem <errjem2@gmail.com>
2023-09-27 17:59:17 +0100

    commit

--- src/main/ProjectsData.jsx
+++ src/main/ProjectsData.jsx
@@ -1,4 +1,5 @@
 import story_corner_img from "./images/storycorner.jpg";
+import color_code_img from "./images/colorcd.jpg";
 import solar_system_img from "./images/solarsystem.jpg";
 import liverpool_fc_img from "./images/liverpool.jpg";
 
@@ -9,6 +10,13 @@ const ProjectsData = [
     image: story_corner_img,
     url: "https://story-corner.netlify.app/",
     code: "https://github.com/Errjem/story-corner",
+  },
+  {
+    title: "Color Code",
+    description: "Discover color's hexadecimal code instantly.",
+    image: color_code_img,
+    url: "https://colorcd.netlify.app/",
+    code: "https://github.com/Errjem/color-code",
   },
   {
     title: "Solar System",
