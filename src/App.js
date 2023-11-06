c62b79dc162eb60a1b57ef212bfa2ef53f032160
houssame errjem <errjem2@gmail.com>
2023-11-06 17:26:54 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Top from "./main/Top";
@@ -7,20 +7,32 @@ import About from "./main/About";
 import Contact from "./main/Contact";
 import { ThemeProvider } from "./main/ThemeContext";
 import Services from "./main/Services";
+import Loading from "./main/Loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <div className="App">
-      <ThemeProvider>
-        <Top />
-        <Home />
-        <About />
-        <Services />
-        <Contact />
-      </ThemeProvider>
+      {loading ? (
+        <Loading />
+      ) : (
+        <ThemeProvider>
+          <Top />
+          <Home />
+          <About />
+          <Services />
+          <Contact />
+        </ThemeProvider>
+      )}
     </div>
   );
 }
---
+++ src/main/Loading.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
